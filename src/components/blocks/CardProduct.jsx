44f271147ca80a7206bf6cbcbdae0764b1ceb5bf
salma <luthfiatiwirona2005@gmail.com>
2025-09-23 18:21:58 +0700

    add api

--- src/components/blocks/CardProduct.jsx
+++ src/components/blocks/CardProduct.jsx
@@ -3,6 +3,21 @@ import ButtonOrder from "../ui/ButtonOrder";
 import { Link } from "react-router";
 
 export default function CardProduct({ price }) {
+    const [product, setProducts] = useState(null)
+
+    useEffect(function () {
+        fetch('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiaq0vtiqC5lUbp5QISKsCNKo78IlthRwKORMlZm4ARcdBW_mq-kBQBQ0TumD7ZK7AyvpSwWU7Zu6X9W1K0HonSQq9W9KIPckCzH7O0q8b0Y7CPbPRp6ApfVOeD2wVJuyREx7qUaApc2IGoEMH_XxW00mpuAaT1H9WWcOI-M_TkzY8o_ZKL9ra1EweE9f1McffhD2WiMjL8-yYj32hEJlpIaBF3Rg5KuO84Lud3EduRBmchX6GM7HwX6awzVscdpYcEe5_73KOoTDn1nUSqaHiXMslrcbRHAGCQoahy&lib=MRwva2RODuoJKcY1OrnynVx_6vdbiN_37')
+            .then(function (response) {
+                console.log(response)
+                return response.json()
+            })
+            .then(function (response) {
+                console.log("data", response)
+                setProducts(response)
+            })
+    })
+
+
 
     return (
         <Container>
