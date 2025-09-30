6176c7dbb56b85f5ced609058fadf8f0501f1423
luthfiatiwi <luthfiatiwirona2005@gmail.com>
2025-09-30 20:25:30 +0700

    responsive-navbar

--- src/components/blocks/Navbar.jsx
+++ src/components/blocks/Navbar.jsx
@@ -1,29 +1,21 @@
-import Search from "../icons/Search";
 import Container from "../layout/Container";
-import Input from "../ui/Input";
 
 export default function Navbar() {
     return (
-        <nav className="bg-white shadow sticky top-0 py-2 px-4 z-50">
-            <Container className={"flex justify-between items-center px-4 py-2"}>
-
+        <nav className="bg-white shadow">
+            <Container className={"flex flex-col gap-3 sm:flex-row sm:justify-between sm:items-center px-4 py-2"}>
                 {/* Logo */}
-                <a href="/" className="text-3xl font-serif text-red-900 font-bold hover:opacity-50 transition-all">Flash.Print</a>
-
+                <h5 className="text-3xl font-serif text-red-900 mb-2 sm:mb-0 text-center sm:text-left">Flash.Print</h5>
                 {/* Search Bar */}
-                <div className="relative flex items-center w-[900px] font-serif">
-                    <Input type="text"
+                <div className="relative flex items-center w-full max-w-full sm:max-w-xl">
+                    <input type="text"
                         placeholder="Anda Ingin Cetak Apa ?"
-                        className=" px-4 py-1" />
-                    <button className="absolute right-0 top-0 h-full px-4 text-white bg-red-900 hover:bg-white hover:text-red-900 border-4 border-red-900 rounded-r-md flex items-center justify-center">
-                        <span className="flex items-center gap-1">
-                            <Search />
-                            Cari</span>
+                        className="w-full sm:w-80 px-4 py-1 text-white bg-red-800 rounded-md focus:outline-none placeholder-white " />
+                    <button className="absolute right-0 top-0 h-full px-4 text-white bg-red-950 rounded-r-md flex items-center justify-center hover:bg-red-700">
+                        <span className="ml-2 hidden sm:inline">Cari</span>
                     </button>
                 </div>
-
-
             </Container>
-        </nav >
-    )
+        </nav>
+    );
 }
